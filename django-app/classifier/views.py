44b81f1ec63717c6ca8741a7a9434caadcc11274
youichiro <cinnamon416@gmail.com>
2017-04-25 15:56:26 +0900

    見た目を編集

--- django-app/classifier/views.py
+++ django-app/classifier/views.py
@@ -8,6 +8,25 @@ def form(request):
     cls = NaiveBayes()
     if request.POST:
         post_url = request.POST['url']
-        category = cls.classify(post_url)
+        category_en = cls.classify(post_url)
+        if category_en == "column":
+            category = "コラム"
+        elif category_en == "overseas":
+            category = "海外"
+        elif category_en == "funny":
+            category = "おもしろ"
+        elif category_en == "domestic":
+            category = "国内"
+        elif category_en == "sports":
+            category = "スポーツ"
+        elif category_en == "gourment":
+            category = "グルメ"
+        elif category_en == "entertainment":
+            category = "エンタメ"
+        elif category_en == "it_science":
+            category = "IT・科学"
+        else:
+            category = ""
+
     return render(request, 'classifier/form.html', {'form_style': form_style, 'category': category})
 
